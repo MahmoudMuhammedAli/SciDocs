78a79c10cf449efbc16790e66bcd933310054147
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-08-07 01:04:07 +0200

    run socket server on env port

--- server/server.js
+++ server/server.js
@@ -1,42 +1,47 @@
-const mongoose = require("mongoose")
-const Document = require("./Document")
+const mongoose = require("mongoose");
+const Document = require("./Document");
+const express = require("express");
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+
+// run app on port 3001 or env port
+const port = process.env.PORT || 3002;
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 mongoose.connect("mongodb://localhost/google-docs-clone", {
-  useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-})
+  useNewUrlParser: true,
+});
 
-const io = require("socket.io")(3001, {
+const server = http.createServer(app);
+const io = socketio(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: "*",
   },
-})
+});
+const defaultValue = "";
 
-const defaultValue = ""
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
 
-io.on("connection", socket => {
-  socket.on("get-document", async documentId => {
-    const document = await findOrCreateDocument(documentId)
-    socket.join(documentId)
-    socket.emit("load-document", document.data)
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
 
-    socket.on("send-changes", delta => {
-      socket.broadcast.to(documentId).emit("receive-changes", delta)
-    })
-
-    socket.on("save-document", async data => {
-      await Document.findByIdAndUpdate(documentId, { data })
-    })
-  })
-})
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
 
 async function findOrCreateDocument(id) {
-  if (id == null) return
+  if (id == null) return;
 
-  const document = await Document.findById(id)
-  if (document) return document
-  return await Document.create({ _id: id, data: defaultValue })
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
 }
