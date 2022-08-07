19435c7395142ede8e11ae4813f8811c2eb35914
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-08-07 06:32:44 +0200

    create a new react app

--- client2/src/pages/Login.js
+++ client2/src/pages/Login.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
-
+import Lottie from "lottie-react";
+import typing from "../assets/typing.json";
+import working from "../assets/working.json";
 export default function Login() {
   return (
-    <div className="text-3xl font-bold underline hidden">
+    <div className="text-3xl font-bold underline ">
       <Typewriter
         onInit={(typewriter) => {
           typewriter
@@ -14,6 +16,8 @@ export default function Login() {
         }}
       />
       <h1 className="underline text-red-200">oh tailwind my tailwind</h1>
+      <Lottie animationData={typing} loop={false} />
+      <Lottie animationData={working} loop={false} />
     </div>
   );
 }
