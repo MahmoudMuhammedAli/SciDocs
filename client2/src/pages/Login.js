49a4a921ebc49246eff26c25b35f17c9049b563f
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-08-07 07:40:58 +0200

    working on the login screen

--- client2/src/pages/Login.js
+++ client2/src/pages/Login.js
@@ -5,19 +5,56 @@ import typing from "../assets/typing.json";
 import working from "../assets/working.json";
 export default function Login() {
   return (
-    <div className="text-3xl font-bold underline ">
-      <Typewriter
-        onInit={(typewriter) => {
-          typewriter
-            .typeString("Hello World!")
-            .callFunction(() => {})
-            .pauseFor(2500)
-            .start();
-        }}
-      />
-      <h1 className="underline text-red-200">oh tailwind my tailwind</h1>
-      <Lottie animationData={typing} loop={false} />
-      <Lottie animationData={working} loop={false} />
+    <div className="min-h-screen ">
+      <div className="hidden lg:flex flex-row p-5 justify-between items-center">
+        <h1 className="text-orange-600 text-xl font-bold flex flex-row items-end gap-1 ">
+          SciDox <span className="text-red-600 text-lg">Editor</span>
+        </h1>
+      </div>
+      <div className=" flex flex-col lg:flex-row justify-evenly items-center min-h-full pt-20 lg:pt-0">
+        <div className=" flex w-full lg:w-[80%] justify-center p-10 lg:p-0">
+          <div className="flex flex-col justify-center items-start gap-2">
+            <h1 className="text-orange-600 text-4xl lg:text-7xl font-bold flex flex-row items-end gap-3 w-full mb-2 text-center lg:text-start ">
+              SciDox{" "}
+              <span className="text-red-600 text-2xl lg:text-5xl">
+                <Typewriter
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString("Editor")
+                      .callFunction(() => {})
+                      .pauseFor(2500)
+                      .start()
+                      .deleteAll()
+                      .start();
+                  }}
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </span>
+            </h1>
+            <p className="text-gray-800 text-lg">
+              Collaborate on scientific documents and share them with the world.
+            </p>
+            <h2 className="text-3xl text-red-600 text-center font-semibold w-full mt-9">
+              Get Started Now
+            </h2>
+            <p className="text-gray-900 text-lg ">Enter your name</p>
+            <input
+              id="name"
+              type="text"
+              placeholder="Name"
+              class="input w-full max-w-xs input-ghost input-bordered"
+            />
+          </div>
+        </div>
+        <div className="flex  lg:w-[70%] justify-center items-end">
+          <div className="w-[70%]">
+            <Lottie animationData={working} loop={true} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
